329b988b55bd4bea8475b52b145d928f6fca750f
sirikhalsa <sirikhalsanm@gmail.com>
2023-03-28 18:57:17 -0600

    Added genre selection prompt (incomplete)

--- game-baja.py
+++ game-baja.py
@@ -14,21 +14,21 @@ greeting = ('Hello, I am Game-Baja, a chatbot designed to help you find video ga
 print(title)
 print(greeting)
 
-begin = input('First let\'s make sure that your keyboard is working. Please press Y and then enter to continue. Or press N and then enter to quit the program. \n')
+begin = input('First let\'s make sure that your keyboard is working. Please press Y and then enter to continue. Or press N and then enter to quit the program. ')
 
-if str(begin) == 'Y':
-    print('Great!')
-elif begin == 'N':
+if begin.lower() == 'y':
+    print('\nGreat!')
+elif begin.lower() == 'n':
     print('It was nice to meet you, I\'m here to help if you need anything else!\n')
 else:
-    while begin != 'Y' and begin != 'N':
+    while begin.lower() != 'y' and begin.lower() != 'n':
         print('You entered something other than Y or N.')
         begin = input('Please press Y or N and then hit enter. ')
         if begin == 'Y':
             print('Great!')
             break
 
-user_name = input('\nFirst things first, what is your name?')
+user_name = input('\nFirst things first, what is your name? ')
 print('\nNice to meet you {name}! You can call me GB.'.format(name = user_name))
 
 console_num = int(input('''\nSo {name}, what console are you currently playing on? Please choose the number of associated console below:
@@ -56,3 +56,38 @@ else:
             console_str = val
 
     print('That\'s great, I love the {console}!'.format(console = console_str))
+
+genre_num_str = input('''\nWe have plenty of games in our database for {console}. Why don\'t we start to narrow the list down a little by choosing some styles of games you enjoy?\n
+Please select one or more genres from the list below by inputting the numbers of the associated genres (separate numbers by a comma for multiple selections):
+1. Action/Adventure
+2. Shooters (FPS and TPS)
+2. Role Playing Games (RPGs, ARPGs and JRPGs)
+3. Real-time Strategy (RTS)
+4. Sports
+5. Puzzles
+6. Survival/Horror
+Please input the number(s) here: '''.format(console = console_str))
+
+genre_num_list = genre_num_str.split(',')
+
+genre_dict = {
+1 : 'Action/Adventure',
+2 : 'Shooters (FPS and TPS)',
+2 : 'Role Playing Games (RPGs, ARPGs and JRPGs)',
+3 : 'Real-time Strategy (RTS)',
+4 : 'Sports',
+5 : 'Puzzles',
+6 : 'Survival/Horror'
+}
+
+genre_list = ""
+
+for genre_num in genre_num_list:
+    for key, val in genre_dict.items():
+        if genre_num == key:
+            genre_list += val
+            genre_list += ', '
+
+print(genre_dict)
+print(genre_num_list)
+print(genre_list)
